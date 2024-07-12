d02e4d1acdc1ef6c4eb0076f192a671b66482cf5
Shane Barton <shanecb@hotmail.ca>
2024-07-12 11:52:30 -0400

    Create function to generate cpu choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,6 @@
-console.log("Hello World!");
+function getComputerChoice(){
+    let choice = Math.floor(Math.random() * 3) + 1;
+    return choice === 1 ? "rock" : choice === 2 ? "paper" : "scissors";
+}
+
+console.log(getComputerChoice());
